66619ec0ef371f5655494f1a2d8cd6b47c17dd05
KavinduPathiratne09 <kavindupathiratne@gmail.com>
2024-06-25 09:30:05 +0530

    Practical 02-02 new

--- Practical02-2.scala
+++ Practical02-2.scala
@@ -1,32 +1,46 @@
-object ExpressionEvaluation {
-  def main(args: Array[String]): Unit = {
-    var a: Int = 2
-    var b: Int = 3
-    var c: Int = 4
-    var d: Int = 5
-    var k: Float = 4.3f
+object ScalaExpressions {
 
-    val a_expr = (-(-b) * a) + (c * d)
-    println(s"a) ${a_expr}")
+  var a = 2
+  var b = 3
+  var c = 4
+  var d = 5
+  var k = 4.3f
+  var g = 0
 
+  def evaluateExpressionA(): Unit = {
+    b -= 1
+    val resultA = b * a + c * d
+    d -= 1
+    println(resultA)
+  }
 
-    println(s"b) ${a}")
-    a += 1
-
-
-    val g: Float = 5.0f 
-    val c_expr = -2 * (g - k) + c
-    println(s"c) ${c_expr}")
-
-
-    println(s"d) ${c}")
-    c += 1
-
-
-    c += 1
-    val e_expr = c * a
-    println(s"e) ${e_expr}")
+  def evaluateExpressionB(): Unit = {
+    println(a)
     a += 1
   }
-}
 
+  def evaluateExpressionC(): Unit = {
+    println(-2 * (g - k) + c)
+  }
+
+  def evaluateExpressionD(): Unit = {
+    val tempC = c
+    c += 1
+    println(tempC)
+  }
+
+  def evaluateExpressionE(): Unit = {
+    c += 1
+    val resultE = c * a
+    a += 1
+    println(resultE)
+  }
+
+  def main(args: Array[String]): Unit = {
+    evaluateExpressionA()
+    evaluateExpressionB()
+    evaluateExpressionC()
+    evaluateExpressionD()
+    evaluateExpressionE()
+  }
+}
